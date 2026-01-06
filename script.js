e00edca5b8844a940b24f7fa4c008ddae91c9d9b
subham <subham.mcabhu24@gmail.com>
2026-01-06 14:30:03 +0530

    css updation

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
 
     $('#menu').click(function () {
@@ -42,7 +41,22 @@ checkbox.addEventListener("change", () => {
         }, 500, 'linear')
     });
 
-  
+    // <!-- emailjs to mail contact form data -->
+    $("#contact-form").submit(function (event) {
+        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+
+        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
+            .then(function (response) {
+                console.log('SUCCESS!', response.status, response.text);
+                document.getElementById("contact-form").reset();
+                alert("Form Submitted Successfully");
+            }, function (error) {
+                console.log('FAILED...', error);
+                alert("Form Submission Failed! Try Again");
+            });
+        event.preventDefault();
+    });
+    // <!-- emailjs to mail contact form data -->
 
 });
 
@@ -238,3 +252,4 @@ const checkbox = document.getElementById("checkbox");
 checkbox.addEventListener("change", () => {
   alert("Feature under development");
 });
+
